1baf1d50ac9660c678925a5ec907ad99ea37ceb1
hongmo sung <tesung30@naver.com>
2019-12-28 22:29:00 +0900

    User fix commit

--- src/api/User/seeUser/seeUser.js
+++ src/api/User/seeUser/seeUser.js
@@ -3,14 +3,16 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
     Query: {
         seeUser:  async (_, args, {request, isAuthenticated}) => {
-            isAuthenticated(request);
-            const {id} = args;
-            const user = await prisma.user({id});
-            const posts = await prisma.user({id}).posts();
-            return {
-                user,
-                posts
-            };
+            // isAuthenticated(request);
+            // const {id} = args;
+            // const user = await prisma.user({id});
+            // const posts = await prisma.user({id}).posts();
+            // return {
+            //     user,
+            //     posts
+            // };
+            const {username} = args;
+            return prisma.user({username});
         }
     }
 }
